d4a1fb541ed524c212a374840c8ef0a3b0547803
iislommm <ibragimegamberdiyev81@gmail.com>
2025-10-07 19:05:45 +0500

    db context added

--- src/Infrastructure/Persistence/AppDbContext.cs
+++ src/Infrastructure/Persistence/AppDbContext.cs
@@ -11,6 +11,23 @@ public class AppDbContext : DbContext
     public DbSet<Role> UserRoles { get; set; }
     public DbSet<UserConfirmer> Confirmers { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Banner> Banners { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CaratItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Coupon> Coupons { get; set; }
+    public DbSet<FlashSale> flashSales { get; set; }
+    public DbSet<FlashSaleItem> FlashSaleItems{ get; set; }
+    public DbSet<Order> Orders{ get; set; }
+    public DbSet<OrderItem> OrderItems{ get; set; }
+    public DbSet<PaymentOption> PaymentOptions { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductImage> ProductImages{ get; set; }
+    public DbSet<Review> Reviews{ get; set; }
+    public DbSet<Wishlist> Wishlists { get; set; }
+    public DbSet<WishlistItem> WishlistItems { get; set; }
+
 
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
