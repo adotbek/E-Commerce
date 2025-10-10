e021035932d3b41d95dd8247168c21a3f29ca48e
iislommm <ibragimegamberdiyev81@gmail.com>
2025-10-10 18:33:57 +0500

    updated

---
+++ src/Application/Interfaces/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
+
+public interface IRoleRepository
+{
+    Task<ICollection<User>> GetAllUsersByRoleAsync(string role);
+    Task<List<Role>> GetAllRolesAsync();
+    Task<long> GetRoleIdAsync(string role);
+}
---
+++ src/Infrastructure/Persistence/Repositories/RoleRepository.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Core.Errors;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class RoleRepository(AppDbContext _context) : IRoleRepository
+{
+    public async Task<List<Role>> GetAllRolesAsync() => await _context.UserRoles.ToListAsync();
+
+    public async Task<ICollection<User>> GetAllUsersByRoleAsync(string role)
+    {
+        var foundRole = await _context.UserRoles.Include(u => u.Users).ThenInclude(u => u.Confirmer).FirstOrDefaultAsync(_ => _.Name == role);
+        if (foundRole is null)
+        {
+            throw new EntityNotFoundException(role);
+        }
+        return foundRole.Users;
+    }
+
+    public async Task<long> GetRoleIdAsync(string role)
+    {
+        var foundRole = await _context.UserRoles.FirstOrDefaultAsync(_ => _.Name == role);
+        if (foundRole is null)
+        {
+            throw new EntityNotFoundException(role + " - not found");
+        }
+        return foundRole.Id;
+    }
+}
